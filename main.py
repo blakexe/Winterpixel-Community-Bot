e0c6261c9f799fb552ce2fd17209cc37fe58e9c7
pepperboi <73554898+pepper-boi@users.noreply.github.com>
2022-05-07 12:05:24 -0700

    Bug fixes

--- main.py
+++ main.py
@@ -316,10 +316,10 @@ async def join_game(interaction: discord.Interaction):
 @tree.command()
 async def start_game(interaction: discord.Interaction):
     '''Start a game with the people joined'''
-    if playing:
-        return
     global playing 
     playing = True
+    if playing:
+        return
     response = "Game Starting With: "
     if len(players) <= 1:
         await interaction.response.send_message("Need 2 or more players to start.")
