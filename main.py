2f4de1ba896c3adde65f7e7bc2f082afe4e759e2
pepperboi <73554898+pepper-boi@users.noreply.github.com>
2022-05-10 14:12:33 -0700

    Bug fixes

--- main.py
+++ main.py
@@ -339,7 +339,7 @@ async def start_game(interaction: discord.Interaction):
         await interaction.response.send_message("Need 2 or more players to start.")
         return
     for i in players:
-        response += i + "\n"
+        response += "\n" + i
     embed1=discord.Embed(color=0xa80022)
     embed1.add_field(name="Players: ", value=response, inline=False)
     await interaction.response.send_message(response)
@@ -352,7 +352,8 @@ async def start_game(interaction: discord.Interaction):
             embed.add_field(name="Players: ", value=players[0], inline=False)
             embed.add_field(name="Game:", value=players[0] + " wins!", inline=False)
             money_txt = ""
-            money_txt += i + " " + str(moneys[i]) + "<:coin:910247623787700264>\n"
+            for i in moneys.keys():
+                money_txt += i + " " + str(moneys[i]) + "<:coin:910247623787700264>\n"
             await msg.edit(embed=embed)
             playing = False
             players.clear()
