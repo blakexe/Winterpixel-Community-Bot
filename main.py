646c34f108279500a622b9c280322f06d206a0e4
pepperboi <73554898+pepper-boi@users.noreply.github.com>
2022-06-28 14:14:09 -0700

    Send the test leaderboard

--- main.py
+++ main.py
@@ -602,7 +602,7 @@ async def discord_coins_leaderboard(interaction: discord.Interaction):
    for i in sorted_rankdict:
         message += f"{'#' + str(sorted_rankdict.index(i) + 1):<5} {i[0]:<20} {i[1]:>5,d} 🪙\n"
    message += "```"
-   await interaction.response.send_message("test")
+   await interaction.response.send_message(message)
 #    embed=discord.Embed(color=0xffd700, title="Discord Coins Leaderboard", description=message)
 #    await interaction.response.send_message()
 
