d97bb6b87cf95a642e924dc029ea59c6fd6c5c33
pepperboi <73554898+pepper-boi@users.noreply.github.com>
2022-06-28 16:11:04 -0700

    Get keys as string

--- main.py
+++ main.py
@@ -110,11 +110,11 @@ def generate_random_name():
     
 def add_player_coin(player, coins):
     if type(player) is int:
-        player_coins = db.get(player)
+        player_coins = db.get(str(player))
         if player_coins == None:
-            db[player] = {"name":client.get_user(player),"money":500, "inventory":{}}
+            db[str(player)] = {"name":client.get_user(player),"money":500, "inventory":{}}
         db[player]["money"] = db[player]["money"] + coins
-        return db[player]
+        return db[str(player)]["money"]
     return 0
 
 def basic_or_elite(a, b, c):
