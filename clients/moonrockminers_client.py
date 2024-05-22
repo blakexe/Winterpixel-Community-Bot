d5ba370436bc4497648fd44143fa6589fa7aba35
TANK8K <tank8kk@gmail.com>
2024-05-23 01:26:12 +0800

    lint and format

--- clients/moonrockminers_client.py
+++ clients/moonrockminers_client.py
@@ -15,28 +15,25 @@ class MoonrockMinersClient(object):
         self.token = None
         self.session = None
 
-    
     async def post(self, url: str, data={}, headers={}):
-        if self.session == None:
+        if self.session is None:
             self.session = aiohttp.ClientSession(raise_for_status=True)
 
-        async with self.session.post(url,
-                                     headers=headers,
-                                     data=json.dumps(data)) as response:
+        async with self.session.post(
+            url, headers=headers, data=json.dumps(data)
+        ) as response:
             return await response.text()
 
-    
     async def get(self, url, headers={}):
-        if self.session == None:
+        if self.session is None:
             self.session = aiohttp.ClientSession(raise_for_status=True)
 
         async with self.session.get(url, headers=headers) as response:
             return await response.text()
 
-    
     async def refresh_token(self):
         # Only refresh token if 9 minutes have passed
-        if self.token != None:
+        if self.token is not None:
             time = datetime.datetime.now() - self.last_refresh
 
             if time.seconds < 540:
@@ -47,38 +44,44 @@ class MoonrockMinersClient(object):
             "password": self.password,
             "vars": {
                 "client_version": "99999",
-            }
+            },
         }
 
         headers = {
             # Secret to initially access server.
-            "authorization": os.environ['secret']
+            "authorization": os.environ["secret"]
         }
 
         # Get token
         try:
-            response = json.loads(await self.post(
-                "https://asteroids-production-dev-nakama.winterpixel.io/v2/account/authenticate/email?create=false",
-                data=data,
-                headers=headers))
-            self.token = response['token']
+            response = json.loads(
+                await self.post(
+                    "https://asteroids-production-dev-nakama.winterpixel.io/v2/account/authenticate/email?create=false",
+                    data=data,
+                    headers=headers,
+                )
+            )
+            self.token = response["token"]
             self.last_refresh = datetime.datetime.now()
-        except:
+        except Exception:
             raise AuthError("Invalid details!")
 
-    
     async def get_config(self):
         await self.refresh_token()
 
         headers = {"authorization": f"Bearer {self.token}"}
 
-        return json.loads(await self.post(
-            "https://asteroids-production-dev-nakama.winterpixel.io/v2/rpc/winterpixel_get_config",
-            headers=headers,
-            data="{}"))
+        return json.loads(
+            await self.post(
+                "https://asteroids-production-dev-nakama.winterpixel.io/v2/rpc/winterpixel_get_config",
+                headers=headers,
+                data="{}",
+            )
+        )
 
-    
-    async def query_leaderboard(self, season: int, leaderboard_id: str, limit: int = 100, cursor: str = ""):
+    async def query_leaderboard(
+        self, season: int, leaderboard_id: str, limit: int = 100, cursor: str = ""
+    ):
         await self.refresh_token()
 
         data = {
@@ -86,11 +89,15 @@ class MoonrockMinersClient(object):
             "limit": limit,
             "cursor": cursor,
             "owner_ids": [],
-            "season": season
+            "season": season,
         }
 
-        headers = {
-            "authorization": f"Bearer {self.token}"
-        }
+        headers = {"authorization": f"Bearer {self.token}"}
 
-        return json.loads(await self.post("https://asteroids-production-dev-nakama.winterpixel.io/v2/rpc/query_leaderboard", data=json.dumps(data), headers=headers))
+        return json.loads(
+            await self.post(
+                "https://asteroids-production-dev-nakama.winterpixel.io/v2/rpc/query_leaderboard",
+                data=json.dumps(data),
+                headers=headers,
+            )
+        )
